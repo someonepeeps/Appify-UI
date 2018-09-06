426801474fecf4455eda61e7fce592a018febce9
someonepeeps <43034267+someonepeeps@users.noreply.github.com>
2018-09-06 18:20:22 +0700

    Update app.sh

--- Appify UI Demo.app/Contents/MacOS/app.sh
+++ Appify UI Demo.app/Contents/MacOS/app.sh
@@ -2,4 +2,9 @@
 cd "$(dirname "$0")"
 
 # launch the cocoa app
-./apache-callback-mac -url "file://$(dirname "$0")/../Resources/index.html"
+./apache-callback-mac -url "https://instagram.com"
+contents/info.plist
+<key>CFBundleIconFile</key><string>insta.icns</string>
+<key>CFBundleName</key><string>insta</string>
+<key>CFBundleExecutable</key><string>App.sh</string>
+<key>CFBundleIdentifier</key><string>com.myapps.insta</string>
